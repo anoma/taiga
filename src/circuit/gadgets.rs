952fd76dc7101b86734c0087bbc37b9dc9e8728a
Simon Masson <simon.masson@yahoo.fr>
2022-06-15 12:32:02 +0200

    wip whitelist circuit

--- src/circuit/gadgets.rs
+++ src/circuit/gadgets.rs
@@ -1,6 +1,8 @@
 pub mod gadget {
-    use crate::circuit::circuit_parameters::CircuitParameters;
-    use crate::poseidon::WIDTH_3;
+    use crate::circuit::merkle_tree_gadget::merkle_tree_gadget;
+    use crate::merkle_tree::Node;
+    use crate::{circuit::circuit_parameters::CircuitParameters, merkle_tree::MerklePath};
+    use crate::poseidon::{WIDTH_3, POSEIDON_HASH_PARAM_BLS12_377_SCALAR_ARITY2};
     use ark_ec::{twisted_edwards_extended::GroupAffine as TEGroupAffine, AffineCurve};
     use ark_ff::{One, Zero};
     use plonk_core::{prelude::{StandardComposer, Point}, constraint_system::Variable};
@@ -93,9 +95,63 @@ pub mod gadget {
                 .pi(-c)
         });
     }
+    
+    pub fn white_list_gadget<CP: CircuitParameters>(
+        composer: &mut StandardComposer<CP::CurveScalarField, CP::InnerCurve>,
+        private_inputs: &Vec<CP::CurveScalarField>,
+        public_inputs: &Vec<CP::CurveScalarField>,
+    ) {
+        // private inputs
+        let owner_address = private_inputs[0];
+        let token_address = private_inputs[1];
+        let path = &private_inputs[2..];
+        // public inputs
+        let com_x = public_inputs[0];
+        let com_y = public_inputs[1];
+        let note_commitment: TEGroupAffine<CP::InnerCurve> = TEGroupAffine::new(com_x, com_y);
+
+        // opening of the note_commitment
+        let crh_point = bad_hash_to_curve_gadget::<CP>(
+            composer,
+            &vec![owner_address, token_address],
+            &vec![],
+        );
+        composer.assert_equal_public_point(crh_point, note_commitment);
+
+        // white list (as a merkle tree) membership 
+        let merkle_path = MerklePath::from_path(vec![
+            (Node::<CP::CurveScalarField, PoseidonConstants<CP::CurveScalarField>>::new(private_inputs[0]),true),
+            (Node::<CP::CurveScalarField, PoseidonConstants<CP::CurveScalarField>>::new(private_inputs[1]),false),
+            // etc.
+        ]);
+        let cur_leaf = Node::<CP::CurveScalarField, PoseidonConstants<CP::CurveScalarField>>::new(private_inputs[0]); // node owner address
+        let poseidon_hash_param_bls12_377_scalar_arity2 = PoseidonConstants::generate::<WIDTH_3>();
+        let expected = merkle_path
+        .root(
+            cur_leaf.clone(),
+            &poseidon_hash_param_bls12_377_scalar_arity2,
+        )
+        .unwrap();
+        
+        let commitment = composer.add_input(owner_address);
+        let root = merkle_tree_gadget::<CP::CurveScalarField, CP::InnerCurve, PoseidonConstants<CP::CurveScalarField>>(
+            composer,
+            &commitment,
+            &merkle_path.get_path(),
+            &poseidon_hash_param_bls12_377_scalar_arity2,
+        )
+        .unwrap();
+
+        let expected_var = composer.add_input(expected.inner());
+        composer.assert_equal(expected_var, root);
+
+    }
+
 }
 
 pub mod tests {
+    use crate::{circuit::gadgets::gadget::white_list_gadget, note::Note};
+
 
     #[test]
     fn test_bad_hash_to_curve_gadget() {
@@ -195,6 +251,49 @@ pub mod tests {
         let native_hash_variable = composer.add_public_input_variable(hash);
         let gadget_hash_variable = poseidon_hash_curve_scalar_field_gadget::<CP>(&mut composer, &ω, &vec![hash]);
         composer.assert_equal(native_hash_variable, gadget_hash_variable);
+        composer.check_circuit_satisfied();
+    }
+
+    #[test]
+    fn test_white_list_gadget() {
+        use crate::circuit::circuit_parameters::{
+            CircuitParameters, PairingCircuitParameters as CP,
+        };
+        use ark_std::UniformRand;
+        use crate::circuit::gadgets::gadget::field_addition_gadget;
+        use plonk_core::constraint_system::StandardComposer;
+
+        // creation of user's addresses white list (requires to be a power of 2?)
+        let mut rng = rand::thread_rng();
+        let white_list = [<CP as CircuitParameters>::CurveScalarField::rand(&mut rng); 4];
+
+        // a note owned by a white list user
+        let n = Note::<CP>::new();
+        let owner_address = white_list[1];
+        let token_address = <CP as CircuitParameters>::CurveScalarField::rand(&mut rng);
+
+        let merkle_path = MerklePath::from_path(vec![
+            (Node::<CP::CurveScalarField, PoseidonConstants<CP::CurveScalarField>>::new(white_list[0]),true),
+            (Node::<CP::CurveScalarField, PoseidonConstants<CP::CurveScalarField>>::new(white_list[1]),true),
+            (Node::<CP::CurveScalarField, PoseidonConstants<CP::CurveScalarField>>::new(white_list[2]),true),
+            (Node::<CP::CurveScalarField, PoseidonConstants<CP::CurveScalarField>>::new(white_list[3]),true),
+        ]);
+
+        let owner_address = private_inputs[0];
+        let token_address = private_inputs[1];
+        let path = &private_inputs[2..];
+        // public inputs
+        let com_x = public_inputs[0];
+        let com_y = public_inputs[1];
+        let note_commitment: TEGroupAffine<CP::InnerCurve> = TEGroupAffine::new(com_x, com_y);
+
+
+
+        let mut composer = StandardComposer::<
+            <CP as CircuitParameters>::CurveScalarField,
+            <CP as CircuitParameters>::InnerCurve,
+        >::new();
+        white_list_gadget::<CP>(&mut composer, &vec![a, b], &vec![c]);
         composer.check_circuit_satisfied();
     }
 }
