7293ea9db675f53b112afbd6facc41a9ad79ca65
Simon Masson <simon.masson@yahoo.fr>
2022-06-28 16:23:57 +0200

    file for iraklis

--- src/circuit/gadgets/poseidon_hash_scalar_field.rs
+++ src/circuit/gadgets/poseidon_hash_scalar_field.rs
@@ -1,7 +1,7 @@
 use crate::poseidon::WIDTH_3;
 use ark_ec::TEModelParameters;
 use ark_ff::PrimeField;
-use plonk_core::{constraint_system::Variable, prelude::StandardComposer};
+use plonk_core::prelude::StandardComposer;
 use plonk_hashing::poseidon::{
     constants::PoseidonConstants,
     poseidon::{PlonkSpec, Poseidon},
@@ -13,8 +13,8 @@ pub fn poseidon_hash_curve_scalar_field_gadget<
 >(
     composer: &mut StandardComposer<F, P>,
     private_inputs: &[F],
-    _public_inputs: &[F],
-) -> Variable {
+    public_inputs: &[F],
+) {
     // no public input here
     // private_inputs are the inputs for the Poseidon hash
     let inputs_var = private_inputs
@@ -31,39 +31,51 @@ pub fn poseidon_hash_curve_scalar_field_gadget<
     inputs_var.iter().for_each(|x| {
         let _ = poseidon_circuit.input(*x).unwrap();
     });
-    poseidon_circuit.output_hash(composer)
+    let hash_out_1 = poseidon_circuit.output_hash(composer);
+    let hash_out_2 = composer.add_input(public_inputs[0]);
+    let _ = composer.assert_equal(hash_out_1, hash_out_2);
 }
 
 #[test]
-fn test_poseidon_gadget() {
+fn test_proof_verify() {
     use crate::circuit::circuit_parameters::{CircuitParameters, PairingCircuitParameters as CP};
-    use crate::WIDTH_3;
+    use crate::circuit::validity_predicate::ValidityPredicate;
+    use ark_poly_commit::PolynomialCommitment;
     use ark_std::UniformRand;
-    use plonk_core::constraint_system::StandardComposer;
-    use plonk_hashing::poseidon::constants::PoseidonConstants;
     use plonk_hashing::poseidon::poseidon::NativeSpec;
-    use plonk_hashing::poseidon::poseidon::Poseidon;
 
     type F = <CP as CircuitParameters>::CurveScalarField;
-    type P = <CP as CircuitParameters>::InnerCurve;
+    type InnerC = <CP as CircuitParameters>::InnerCurve;
+    type PC = <CP as CircuitParameters>::CurvePC;
 
+    // hash some random value
     let mut rng = rand::thread_rng();
-    let ω = (0..(WIDTH_3 - 1))
-        .map(|_| F::rand(&mut rng))
-        .collect::<Vec<_>>();
     let poseidon_hash_param_bls12_377_scalar_arity2 = PoseidonConstants::generate::<WIDTH_3>();
-    let mut poseidon = Poseidon::<(), NativeSpec<F, WIDTH_3>, WIDTH_3>::new(
+    let mut poseidon_native = Poseidon::<(), NativeSpec<F, WIDTH_3>, WIDTH_3>::new(
         &mut (),
         &poseidon_hash_param_bls12_377_scalar_arity2,
     );
-    ω.iter().for_each(|x| {
-        poseidon.input(*x).unwrap();
+    let inputs = (0..(WIDTH_3 - 1))
+        .map(|_| F::rand(&mut rng))
+        .collect::<Vec<_>>();
+    inputs.iter().for_each(|x| {
+        let _ = poseidon_native.input(*x).unwrap();
     });
-    let hash = poseidon.output_hash(&mut ());
-    let mut composer = StandardComposer::<F, P>::new();
-    let native_hash_variable = composer.add_public_input_variable(hash);
-    let gadget_hash_variable =
-        poseidon_hash_curve_scalar_field_gadget::<F, P>(&mut composer, &ω, &[hash]);
-    composer.assert_equal(native_hash_variable, gadget_hash_variable);
-    composer.check_circuit_satisfied();
+    let native_hash: F = poseidon_native.output_hash(&mut ());
+
+    // create a setup for a ZK proof using KZG
+    let setup = PC::setup(1 << 9, None, &mut rng).unwrap();
+
+    // create the VP
+    let vp = ValidityPredicate::<CP>::new(
+        &setup,
+        poseidon_hash_curve_scalar_field_gadget::<F, InnerC>,
+        &inputs,
+        &[native_hash],
+        false, // do you want to blind?
+        &mut rng,
+    );
+
+    // verify the VP
+    vp.verify();
 }
