cdffa544bafce289fa0701db3eb001a46811a2de
Song Xuyang <xuyangsong@hotmail.com>
2022-07-11 17:58:02 +0800

    fix pi order in action_circuit_test

--- src/circuit/action_circuit.rs
+++ src/circuit/action_circuit.rs
@@ -108,8 +108,8 @@ fn action_circuit_test() {
 
     // Check the public inputs
     let mut expect_pi = PublicInputs::new(action_circuit.padded_circuit_size());
-    expect_pi.insert(10781, action.root);
-    expect_pi.insert(24766, action.nf.inner());
+    expect_pi.insert(24766, action.root);
+    expect_pi.insert(10781, action.nf.inner());
     expect_pi.insert(31822, action.cm.inner());
     assert_eq!(pi, expect_pi);
     // Verifier
