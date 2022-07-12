448785504b829f05eecbbbba61638762e7654077
Simon Masson <simon.masson@yahoo.fr>
2022-07-12 11:39:48 +0200

    changes on tests.rs

--- src/tests.rs
+++ src/tests.rs
@@ -9,7 +9,10 @@ use crate::circuit::circuit_parameters::PairingCircuitParameters as CP;
 use crate::{note::Note, token::Token, user::User};
 use ark_ff::Zero;
 use ark_std::UniformRand;
+use plonk_core::circuit::{Circuit, VerifierData, verify_proof};
+use plonk_core::proof_system::pi::PublicInputs;
 use plonk_hashing::poseidon::constants::PoseidonConstants;
+use ark_poly_commit::PolynomialCommitment;
 
 #[test]
 fn test_send() {
@@ -18,6 +21,8 @@ fn test_send() {
     // compute the (partial) action circuit corresponding to sending the note to the second user
     // the note commitment is stored in a merkle tree for the action proof.
     type Fr = <CP as CircuitParameters>::CurveScalarField;
+    type PC = <CP as CircuitParameters>::CurvePC;
+    type P = <CP as CircuitParameters>::InnerCurve;
 
     //Create global structures
     let mut rng = ark_std::test_rng();
@@ -62,187 +67,27 @@ fn test_send() {
     );
     let action_info = ActionInfo::<CP>::new(spend_info, output_info);
     let (action, mut action_circuit) = action_info.build(&mut rng).unwrap();
-    // we could create the action proof and verify it here (see `src/circuit/action_circuit.rs` for details).
+
+    // Proof computation
+    let pp = PC::setup(action_circuit.padded_circuit_size(), None, &mut rng).unwrap();
+    let (pk_p, vk) = action_circuit.compile::<PC>(&pp).unwrap();
+    let (proof, pi) = action_circuit.gen_proof::<PC>(&pp, pk_p, b"Test").unwrap();
+    let mut expect_pi = PublicInputs::new(action_circuit.padded_circuit_size());
+    expect_pi.insert(24766, action.root);
+    expect_pi.insert(10781, action.nf.inner());
+    expect_pi.insert(31822, action.cm.inner());
+    assert_eq!(pi, expect_pi);
+    let verifier_data = VerifierData::new(vk, expect_pi);
+    verify_proof::<Fr, P, PC>(&pp, verifier_data.key, &proof, &verifier_data.pi, b"Test").unwrap();
 
     // creation of the Bob's note
-    // TODO
-    let output_notes_and_ec = alice.send(&mut [&note_alice_1xan], vec![(&bob, 1_u32)], &mut rng);
-
-    // create a transaction
-    // TODO
-    let tx: Transaction<CP> = Transaction::new(
-        vec![],
-        vec![(note_a1xan, nullifier)],
-        output_notes_and_ec,
-        &vps,
-    );
-    tx.process(&mut nf_tree, &mut mt_tree, &mut cm_ce_list);
-}
-
-/*
-
-#[test]
-fn test_send_kzg() {
-    test_send::<crate::circuit::circuit_parameters::PairingCircuitParameters>();
-}
-
-fn test_check_proofs<CP: CircuitParameters>() {
-    spawn_user::<CP>("Ferdinand").check_proofs();
-}
-
-#[test]
-fn test_check_proofs_kzg() {
-    test_check_proofs::<crate::circuit::circuit_parameters::PairingCircuitParameters>();
-}
-
-fn split_and_merge_notes_test<CP: CircuitParameters>() {
-    use ark_std::UniformRand;
-    // --- SET UP ---
-
-    //Create global structures
-    let mut rng = ThreadRng::default();
-
-    let mut nf_tree = MerkleTree::<Blake2s>::from_leaves(&[]);
-    let mut mt_tree = MerkleTree::<Blake2s>::from_leaves(&[]);
-    let mut cm_ce_list = Vec::new();
-
-    //Create users and tokens
-    let mut rng = ark_std::test_rng();
-    let yulia = User::<CP>::new(&mut rng);
-    let simon = User::<CP>::new(&mut rng);
-    let xan = Token::<CP>::new(&mut rng);
-
-    //Create VPs for the users and tokens
-    let vps = spawn_trivial_vps(3, &mut rng);
-
-    //Create the initial note
-    let initial_note = Note::<CP>::new(
-        yulia,
+    let note_bob_1xan = Note::<CP>::new(
+        bob,
         xan,
-        4,
-        Nullifier::<CP>::new(CP::CurveScalarField::one()),
-        <CP>::CurveScalarField::zero(),
-        <CP as CircuitParameters>::CurveScalarField::rand(&mut rng),
+        1,
+        action.nf,
+        Fr::rand(&mut rng),
+        Fr::rand(&mut rng),
     );
 
-    add_to_tree(&initial_note.get_cm_bytes(), &mut mt_tree);
-    // let initial_note_ec = yulia.encrypt(&mut rng, &initial_note);
-    // cm_ce_list.push((initial_note.commitment(), initial_note_ec));
-
-    //Prepare the hash for future MTtree membership check
-    let hash_in = Blake2s::hash(&serializable_to_vec(&initial_note.commitment()));
-
-    //Check that the note has been created
-    let proof_mt = mt_tree.proof(&[0]);
-    let root_mt = mt_tree.root().unwrap();
-    assert!(proof_mt.verify(root_mt, &[0], &[hash_in], 1));
-
-    // --- SET UP END ---
-
-    // Split a note between users
-    let split_output_notes = yulia.send(
-        &mut [&initial_note],
-        vec![(&yulia, 1), (&yulia, 1), (&simon, 2)],
-        &mut rng,
-    );
-
-    //Prepare the hashes for future MTtree membership check
-    let mut bytes = serializable_to_vec(&split_output_notes[0].0.commitment());
-    let hash1 = Blake2s::hash(&bytes);
-    bytes = serializable_to_vec(&split_output_notes[1].0.commitment());
-    let hash2 = Blake2s::hash(&bytes);
-    bytes = serializable_to_vec(&split_output_notes[2].0.commitment());
-    let hash3 = Blake2s::hash(&bytes);
-
-    //Prepare the nf hash for future spent notes check
-    let nullifier = Nullifier::derive_native(
-        &yulia.get_nk(),
-        &initial_note.rho,
-        &initial_note.psi,
-        &initial_note.commitment(),
-    );
-    let hash_nf = Blake2s::hash(&nullifier.to_bytes());
-
-    //Create a tx splitting the initial note
-    let tx: Transaction<CP> = Transaction::new(
-        vec![],
-        vec![(initial_note, nullifier)],
-        split_output_notes.clone(),
-        &vps,
-    );
-    tx.process(&mut nf_tree, &mut mt_tree, &mut cm_ce_list);
-
-    //Check the amount of output notes and their values
-    assert_eq!(split_output_notes.len(), 3);
-    assert_eq!(split_output_notes[0].0.value, 1);
-    assert_eq!(split_output_notes[1].0.value, 1);
-    assert_eq!(split_output_notes[2].0.value, 2);
-
-    //Check that the initial note has been spent
-    let proof_nf = nf_tree.proof(&[0]);
-    let root_nf = nf_tree.root().unwrap();
-    assert!(proof_nf.verify(root_nf, &[0], &[hash_nf], 1));
-
-    //Check that the notes has been added to the tree
-    let proof_mt = mt_tree.proof(&[1, 2, 3]);
-    let root_mt = mt_tree.root().unwrap();
-    assert!(proof_mt.verify(root_mt, &[1, 2, 3], &[hash1, hash2, hash3], 4));
-
-    let merge_output_notes = yulia.send(
-        &mut [&split_output_notes[0].0, &split_output_notes[1].0],
-        vec![(&yulia, 2)],
-        &mut rng,
-    );
-
-    //Prepare the hash for future MTtree membership check
-    bytes = serializable_to_vec(&merge_output_notes[0].0.commitment());
-    let hash4 = Blake2s::hash(&bytes);
-
-    //Prepare the nf hash for future spent notes check
-    let nullifier1 = Nullifier::derive_native(
-        &yulia.get_nk(),
-        &split_output_notes[0].0.rho,
-        &split_output_notes[0].0.psi,
-        &split_output_notes[0].0.commitment(),
-    );
-    let nullifier2 = Nullifier::derive_native(
-        &yulia.get_nk(),
-        &split_output_notes[1].0.rho,
-        &split_output_notes[1].0.psi,
-        &split_output_notes[1].0.commitment(),
-    );
-    let hash_nf1 = Blake2s::hash(&nullifier1.to_bytes());
-    let hash_nf2 = Blake2s::hash(&nullifier2.to_bytes());
-
-    //Create a tx splitting the initial note
-    let tx: Transaction<CP> = Transaction::new(
-        vec![],
-        vec![
-            (split_output_notes[0].0, nullifier1),
-            (split_output_notes[1].0, nullifier2),
-        ],
-        merge_output_notes.clone(),
-        &vps,
-    );
-    tx.process(&mut nf_tree, &mut mt_tree, &mut cm_ce_list);
-
-    //Check the amount of notes and their values
-    assert_eq!(merge_output_notes.len(), 1);
-    assert_eq!(merge_output_notes[0].0.value, 2);
-
-    //Check that the notes has been spent
-    let proof_nf = nf_tree.proof(&[1, 2]);
-    let root_nf = nf_tree.root().unwrap();
-    assert!(proof_nf.verify(root_nf, &[1, 2], &[hash_nf1, hash_nf2], 3));
-
-    //Check that the notes has been added to the tree
-    let proof_mt = mt_tree.proof(&[4]);
-    let root_mt = mt_tree.root().unwrap();
-    assert!(proof_mt.verify(root_mt, &[4], &[hash4], 5));
 }
-
-#[test]
-fn split_and_merge_notes_test_kzg() {
-    split_and_merge_notes_test::<crate::circuit::circuit_parameters::PairingCircuitParameters>();
-}
-*/
