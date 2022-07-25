97aad7e9e609bd0f4a1647592adfee253e64b93c
Simon Masson <simon.masson@yahoo.fr>
2022-07-25 16:30:16 +0200

    test for ws_to_te conversion

--- src/utils.rs
+++ src/utils.rs
@@ -1,9 +1,10 @@
-use ark_bls12_377::Fq;
+use ark_bls12_381_new::{Fq, g1::self, G1Projective};
 use ark_ec::{
     short_weierstrass_jacobian::GroupAffine as SWGroupAffine,
-    twisted_edwards_extended::GroupAffine as TEGroupAffine,
+    twisted_edwards_extended::GroupAffine as TEGroupAffine, TEModelParameters, ProjectiveCurve,
 };
-use ark_ff::{field_new, BigInteger, One, PrimeField};
+use ark_ff::{field_new, BigInteger, One, PrimeField, UniformRand};
+use ark_std::test_rng;
 
 pub fn bits_to_fields<F: PrimeField>(bits: &[bool]) -> Vec<F> {
     bits.chunks((F::size_in_bits() - 1) as usize)
@@ -11,17 +12,50 @@ pub fn bits_to_fields<F: PrimeField>(bits: &[bool]) -> Vec<F> {
         .collect()
 }
 
-// warning! Works only for bls12_377
+// warning! Works only for bls12_381_new
 pub fn ws_to_te(
-    p: SWGroupAffine<ark_bls12_377::g1::Parameters>,
-) -> TEGroupAffine<ark_bls12_377::g1::Parameters> {
-    // values available in https://github.com/arkworks-rs/curves/blob/master/bls12_377/src/curves/g1.rs
-    let x = p.x;
-    let y = p.y;
+    p: SWGroupAffine<ark_bls12_381_new::g1::Parameters>,
+) -> TEGroupAffine<ark_bls12_381_new::g1::Parameters> {
+    /*
+    Wikipedia provides a WS->M and M->TE conversion.
+    Here we provide the WS -> TE conversion with few simpliciations.
+
+    (a and b are coefficients of the curve)
+    α = sqrt(x**3+a*x+b)
+    s1 = sqrt(3*α**2+a)
+    s2 = sqrt(3*α+2*s1)
+    coeff = (3*α-2*s1)/(3*α+2*s1)
+
+    u = (x-α) * s2 / y
+    v = (x-α-s1) / (x-α+s1)
+    (u,v) satisfies u**2 + v**2 == 1 + coeff * u**2 * v**2
+
+
+    In the case of bls12_381_new:
+    α = -1
+    s1 = sqrt(3)
+    s2 = sqrt(-3+2*sqrt(3))
+    coeff = (-3-2*sqrt(3)) / (-3+2*sqrt(3))
+    u = (x+1) * s2 / y
+    v = (x+1-s1) / (x+1+s1)
+    */
     let alpha = -Fq::one();
-    let s = field_new!(Fq, "10189023633222963290707194929886294091415157242906428298294512798502806398782149227503530278436336312243746741931");
-    let sqrt_te1a = field_new!(Fq, "23560188534917577818843641916571445935985386319233886518929971599490231428764380923487987729215299304184915158756");
-    let x_te = (x - alpha) * sqrt_te1a / y;
-    let y_te = (s * (x - alpha) - Fq::one()) / (s * (x - alpha) + Fq::one());
-    TEGroupAffine::<ark_bls12_377::g1::Parameters>::new(x_te, y_te)
+    let s1 = field_new!(Fq, "611336158540232733028115263714465872671465435137168183916056024884978835858365189946006184892099852878171309395862");
+    let s2 = field_new!(Fq, "1064881461568443305175032400733120695040834941031583268014086210056237636385831065297199936964360836443404508126238");
+    TEGroupAffine::<ark_bls12_381_new::g1::Parameters>::new((p.x-alpha) * s2 / p.y, (p.x-alpha-s1) / (p.x-alpha+s1))
+}
+
+#[test]
+fn test_ws_to_te() {
+    // todo test here
+    let mut rng = test_rng();
+    for _ in 0..100 {
+        let p_ws: SWGroupAffine<g1::Parameters> = G1Projective::rand(&mut rng).into_affine();
+        let p_te = ws_to_te(p_ws);
+        let x = p_te.x;
+        let y = p_te.y;
+        let a = <g1::Parameters as TEModelParameters>::COEFF_A;
+        let d = <g1::Parameters as TEModelParameters>::COEFF_D;
+        assert_eq!(a*x*x + y*y, Fq::one() + d * x*x*y*y);
+    }
 }
