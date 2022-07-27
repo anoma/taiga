72e5fbf9c1799ed0d4210817485233c48f2b0ea0
Simon Masson <simon.masson@yahoo.fr>
2022-07-27 21:15:37 +0200

    add check_circuit_satisfied and some tests (removed from field_addition

--- src/circuit/blinding_circuit.rs
+++ src/circuit/blinding_circuit.rs
@@ -1,11 +1,19 @@
 use crate::circuit::circuit_parameters::CircuitParameters;
+use crate::circuit::validity_predicate::NUM_NOTE;
+use crate::circuit::vp_examples::balance::BalanceValidityPredicate;
+use crate::note::Note;
 use crate::poseidon::WIDTH_9;
+use crate::utils::ws_to_te;
 use crate::vp_description::ValidityPredicateDescription;
 use ark_ec::twisted_edwards_extended::GroupAffine as TEGroupAffine;
+use ark_ec::{AffineCurve, ProjectiveCurve};
 use ark_ff::UniformRand;
 use ark_ff::{BigInteger, PrimeField};
 use ark_poly::univariate::DensePolynomial;
 use ark_poly_commit::PolynomialCommitment;
+use plonk_core::circuit::{verify_proof, VerifierData};
+use plonk_core::proof_system::pi::PublicInputs;
+
 use plonk_core::{
     circuit::Circuit, constraint_system::StandardComposer, prelude::Error, prelude::Variable,
     proof_system::Blinding,
@@ -95,6 +103,7 @@ where
         // TODO: Constrain com_vp
 
         println!("circuit size: {}", composer.circuit_bound());
+        composer.check_circuit_satisfied();
         Ok(())
     }
 
@@ -126,4 +135,141 @@ impl<CP: CircuitParameters> BlindingCircuit<CP> {
     pub fn get_blinding(&self) -> Blinding<CP::CurveScalarField> {
         self.blinding
     }
+}
+
+#[test]
+fn test_check_zh_commitment() {
+    use crate::circuit::circuit_parameters::PairingCircuitParameters as CP;
+    type Fr = <CP as CircuitParameters>::CurveScalarField;
+    type Fq = <CP as CircuitParameters>::CurveBaseField;
+    type P = <CP as CircuitParameters>::InnerCurve;
+    type OP = <CP as CircuitParameters>::Curve;
+    type PC = <CP as CircuitParameters>::CurvePC;
+    type OPC = <CP as CircuitParameters>::OuterCurvePC;
+    use ark_std::test_rng;
+
+    let mut rng = test_rng();
+
+    // A balance VP
+    let input_notes = [(); NUM_NOTE].map(|_| Note::<CP>::dummy(&mut rng));
+    let output_notes = input_notes.clone();
+    let mut balance_vp = BalanceValidityPredicate::new(input_notes, output_notes);
+    balance_vp
+        .gadget(&mut StandardComposer::<Fr, P>::new())
+        .unwrap();
+
+    // we blind the VP desc
+    let pp = PC::setup(balance_vp.padded_circuit_size(), None, &mut rng).unwrap();
+    let vp_desc = ValidityPredicateDescription::from_vp(&mut balance_vp, &pp).unwrap();
+
+    // the blinding circuit, containing the random values used to blind
+    let blinding_circuit =
+        BlindingCircuit::<CP>::new(&mut rng, vp_desc, &pp, balance_vp.padded_circuit_size())
+            .unwrap();
+
+    let b_l = blinding_circuit.get_blinding().q_l;
+
+    // verifying key with the blinding
+    let (_, vk_blind) = balance_vp
+        .compile_with_blinding::<PC>(&pp, &blinding_circuit.get_blinding())
+        .unwrap();
+
+    // verifying key without blinding
+    let (_, vk_no_blind) = balance_vp.compile::<PC>(&pp).unwrap();
+
+    // extract both qL commitment
+    let q_l_blind = ws_to_te(vk_blind.arithmetic.q_l.0);
+    let q_l_no_blind = ws_to_te(vk_no_blind.arithmetic.q_l.0);
+
+    let zh = blinding_circuit.zh;
+
+    let com_zh = TEGroupAffine::<OP>::new(zh[0], zh[1]);
+
+    assert_eq!(q_l_blind, q_l_no_blind + com_zh.mul(b_l).into_affine());
+}
+
+#[test]
+fn test_blinding_circuit() {
+    // creation of a (balance) VP
+    // creation of the corresponding blinding circuit
+    // checking the blinding circuit
+
+    use crate::circuit::circuit_parameters::PairingCircuitParameters as CP;
+    type Fr = <CP as CircuitParameters>::CurveScalarField;
+    type Fq = <CP as CircuitParameters>::CurveBaseField;
+    type P = <CP as CircuitParameters>::InnerCurve;
+    type OP = <CP as CircuitParameters>::Curve;
+    type PC = <CP as CircuitParameters>::CurvePC;
+    type OPC = <CP as CircuitParameters>::OuterCurvePC;
+    use ark_std::test_rng;
+
+    let mut rng = test_rng();
+
+    // A balance VP
+    let input_notes = [(); NUM_NOTE].map(|_| Note::<CP>::dummy(&mut rng));
+    let output_notes = input_notes.clone();
+    let mut balance_vp = BalanceValidityPredicate::new(input_notes, output_notes);
+    balance_vp
+        .gadget(&mut StandardComposer::<Fr, P>::new())
+        .unwrap();
+
+    // we blind the VP desc
+    let pp = PC::setup(balance_vp.padded_circuit_size(), None, &mut rng).unwrap();
+    let vp_desc = ValidityPredicateDescription::from_vp(&mut balance_vp, &pp).unwrap();
+    let vp_desc_compressed = vp_desc.get_compress();
+
+    // the blinding circuit, containing the random values used to blind
+    let mut blinding_circuit =
+        BlindingCircuit::<CP>::new(&mut rng, vp_desc, &pp, balance_vp.padded_circuit_size())
+            .unwrap();
+
+    // verifying key with the blinding
+    let (_, vk_blind) = balance_vp
+        .compile_with_blinding::<PC>(&pp, &blinding_circuit.get_blinding())
+        .unwrap();
+
+    let blinding_circuit_size = blinding_circuit.padded_circuit_size();
+    let pp_blind = OPC::setup(blinding_circuit_size, None, &mut rng).unwrap();
+
+    let (pk_p, vk) = blinding_circuit.compile::<OPC>(&pp_blind).unwrap();
+
+    // Blinding Prover
+    let (proof, pi) = blinding_circuit
+        .gen_proof::<OPC>(&pp_blind, pk_p, b"Test")
+        .unwrap();
+
+    // Expecting vk_blind(out of circuit)
+    let mut expect_pi = PublicInputs::new(blinding_circuit_size);
+    let q_m = ws_to_te(vk_blind.arithmetic.q_m.0);
+    expect_pi.insert(392, q_m.x);
+    expect_pi.insert(393, q_m.y);
+    let q_l = ws_to_te(vk_blind.arithmetic.q_l.0);
+    expect_pi.insert(782, q_l.x);
+    expect_pi.insert(783, q_l.y);
+    let q_r = ws_to_te(vk_blind.arithmetic.q_r.0);
+    expect_pi.insert(1172, q_r.x);
+    expect_pi.insert(1173, q_r.y);
+    let q_o = ws_to_te(vk_blind.arithmetic.q_o.0);
+    expect_pi.insert(1562, q_o.x);
+    expect_pi.insert(1563, q_o.y);
+    let q_4 = ws_to_te(vk_blind.arithmetic.q_4.0);
+    expect_pi.insert(1952, q_4.x);
+    expect_pi.insert(1953, q_4.y);
+    let q_c = ws_to_te(vk_blind.arithmetic.q_c.0);
+    expect_pi.insert(2342, q_c.x);
+    expect_pi.insert(2343, q_c.y);
+    expect_pi.insert(21388, vp_desc_compressed);
+
+    assert_eq!(pi, expect_pi);
+
+    // Blinding Verifier
+    let verifier_data = VerifierData::new(vk, pi);
+    verify_proof::<Fq, OP, OPC>(
+        &pp_blind,
+        verifier_data.key,
+        &proof,
+        &verifier_data.pi,
+        b"Test",
+    )
+    .unwrap();
 }
