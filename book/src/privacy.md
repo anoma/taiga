3ff528e5bc696740dbee767ee02f11ce8804164e
Simon Masson <simon.masson@yahoo.fr>
2022-08-03 13:44:23 +0200

    new file for doc rust tests

--- book/src/privacy.md
+++ book/src/privacy.md
@@ -1,6 +1,8 @@
 # Full privacy
 
 * What leaks info with custom VPs
-* Randomization of the circuit
-* Blinding proof and commitment to the verifier key
-* The issue with this method, the accumulation alternative.
+* How we fix it: we randomize the verifier keys.
+* In consequence we need to bind randomizedVK to VK. We use another commitment.
+* Blinding proof is split in two parts:
+    * vk and rand leads to randomizedVK,
+    * vk opens vkCommitment.
--- src/doc_test_simple_example.rs
+++ src/doc_test_simple_example.rs
@@ -1,8 +1,8 @@
 use ark_ec::TEModelParameters;
 use ark_ff::PrimeField;
-use plonk_core::prelude::{Circuit, StandardComposer, Error};
+use plonk_core::prelude::{Circuit, Error, StandardComposer};
 
-pub struct AdditionCircuit<F:PrimeField> {
+pub struct AdditionCircuit<F: PrimeField> {
     a: F,
     b: F,
     pub c: F,
@@ -11,15 +11,12 @@ pub struct AdditionCircuit<F:PrimeField> {
 impl<F, P> Circuit<F, P> for AdditionCircuit<F>
 where
     F: PrimeField,
-    P: TEModelParameters<BaseField=F>,
+    P: TEModelParameters<BaseField = F>,
 {
     const CIRCUIT_ID: [u8; 32] = [0x00; 32];
 
     // Default implementation
-    fn gadget(
-        &mut self,
-        composer: &mut StandardComposer<F, P>,
-    ) -> Result<(), Error> {
+    fn gadget(&mut self, composer: &mut StandardComposer<F, P>) -> Result<(), Error> {
         let var_a = composer.add_input(self.a);
         let var_b = composer.add_input(self.b);
         // add a gate for the addition
@@ -58,11 +55,17 @@ fn test_circuit_example() {
     // Circuit
     let mut circuit = AdditionCircuit::<F> { a, b, c };
     // Setup
-    let setup = PC::setup(Circuit::<F, P>::padded_circuit_size(&circuit), None, &mut rng).unwrap();
+    let setup = PC::setup(
+        Circuit::<F, P>::padded_circuit_size(&circuit),
+        None,
+        &mut rng,
+    )
+    .unwrap();
     // Prover and verifier key
     let (pk, vk) = Circuit::<F, P>::compile::<PC>(&mut circuit, &setup).unwrap();
     // Proof computation
-    let (pi, public_inputs) = Circuit::<F, P>::gen_proof::<PC>(&mut circuit, &setup, pk, b"Test").unwrap();
+    let (pi, public_inputs) =
+        Circuit::<F, P>::gen_proof::<PC>(&mut circuit, &setup, pk, b"Test").unwrap();
     // Proof verification
     let verifier_data = VerifierData::new(vk, public_inputs);
     verify_proof::<F, P, PC>(&setup, verifier_data.key, &pi, &verifier_data.pi, b"Test").unwrap();
