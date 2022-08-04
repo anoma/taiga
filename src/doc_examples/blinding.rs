1043f379330243811193b84b9ba6741ce29e030f
Simon Masson <simon.masson@yahoo.fr>
2022-08-04 18:46:32 +0200

    add blinding test but it does not work yet

--- src/doc_examples/blinding.rs
+++ src/doc_examples/blinding.rs
@@ -38,56 +38,56 @@ fn test_blinding_circuit() {
         BlindingCircuit::<CP>::new(&mut rng, vp_desc, &pp, vp.padded_circuit_size()).unwrap();
 
     // verifying key with the blinding
-    let (_, vk_blind) = vp
+    let (_, vk_blinded) = vp
         .compile_with_blinding::<PC>(&pp, &blinding_circuit.get_blinding())
         .unwrap();
 
-    let blinding_circuit_size = blinding_circuit.padded_circuit_size();
-    let pp_blind = Opc::setup(blinding_circuit_size, None, &mut rng).unwrap();
+    let pp_blind = Opc::setup(blinding_circuit.padded_circuit_size(), None, &mut rng).unwrap();
 
-    let (pk_p, vk) = blinding_circuit.compile::<Opc>(&pp_blind).unwrap();
+    // prover and verifier key for the blinding proof
+    let (pk_blinding, vk_blinding) = blinding_circuit.compile::<Opc>(&pp_blind).unwrap();
 
     // Blinding Prover
-    let (proof, pi) = blinding_circuit
-        .gen_proof::<Opc>(&pp_blind, pk_p, b"Test")
+    let (proof, public_inputs) = blinding_circuit
+        .gen_proof::<Opc>(&pp_blind, pk_blinding, b"Test")
         .unwrap();
 
     //
-    // this is very expensive and needs to be adapted
+    // this is not working yet
     //
 
-    // // Expecting vk_blind(out of circuit)
-    // let mut expect_pi = PublicInputs::new(blinding_circuit_size);
-    // let q_m = ws_to_te(vk_blind.arithmetic.q_m.0);
-    // expect_pi.insert(392, q_m.x);
-    // expect_pi.insert(393, q_m.y);
-    // let q_l = ws_to_te(vk_blind.arithmetic.q_l.0);
-    // expect_pi.insert(782, q_l.x);
-    // expect_pi.insert(783, q_l.y);
-    // let q_r = ws_to_te(vk_blind.arithmetic.q_r.0);
-    // expect_pi.insert(1172, q_r.x);
-    // expect_pi.insert(1173, q_r.y);
-    // let q_o = ws_to_te(vk_blind.arithmetic.q_o.0);
-    // expect_pi.insert(1562, q_o.x);
-    // expect_pi.insert(1563, q_o.y);
-    // let q_4 = ws_to_te(vk_blind.arithmetic.q_4.0);
-    // expect_pi.insert(1952, q_4.x);
-    // expect_pi.insert(1953, q_4.y);
-    // let q_c = ws_to_te(vk_blind.arithmetic.q_c.0);
-    // expect_pi.insert(2342, q_c.x);
-    // expect_pi.insert(2343, q_c.y);
-    // expect_pi.insert(21388, vp_desc_compressed);
+    // Expecting vk_blind(out of circuit)
+    let mut expected_public_inputs = PublicInputs::new(blinding_circuit.padded_circuit_size());
+    let q_m = ws_to_te(vk_blinded.arithmetic.q_m.0);
+    expected_public_inputs.insert(392, q_m.x);
+    expected_public_inputs.insert(393, q_m.y);
+    let q_l = ws_to_te(vk_blinded.arithmetic.q_l.0);
+    expected_public_inputs.insert(782, q_l.x);
+    expected_public_inputs.insert(783, q_l.y);
+    let q_r = ws_to_te(vk_blinded.arithmetic.q_r.0);
+    expected_public_inputs.insert(1172, q_r.x);
+    expected_public_inputs.insert(1173, q_r.y);
+    let q_o = ws_to_te(vk_blinded.arithmetic.q_o.0);
+    expected_public_inputs.insert(1562, q_o.x);
+    expected_public_inputs.insert(1563, q_o.y);
+    let q_4 = ws_to_te(vk_blinded.arithmetic.q_4.0);
+    expected_public_inputs.insert(1952, q_4.x);
+    expected_public_inputs.insert(1953, q_4.y);
+    let q_c = ws_to_te(vk_blinded.arithmetic.q_c.0);
+    expected_public_inputs.insert(2342, q_c.x);
+    expected_public_inputs.insert(2343, q_c.y);
+    expected_public_inputs.insert(21388, vp_desc_compressed);
 
-    // assert_eq!(pi, expect_pi);
+    assert_eq!(public_inputs, expected_public_inputs);
 
-    // // Blinding Verifier
-    // let verifier_data = VerifierData::new(vk, pi);
-    // verify_proof::<Fq, OP, Opc>(
-    //     &pp_blind,
-    //     verifier_data.key,
-    //     &proof,
-    //     &verifier_data.pi,
-    //     b"Test",
-    // )
-    // .unwrap();
+    // Blinding Verifier
+    let verifier_data = VerifierData::new(vk_blinding, public_inputs);
+    verify_proof::<Fq, OP, Opc>(
+        &pp_blind,
+        verifier_data.key,
+        &proof,
+        &verifier_data.pi,
+        b"Test",
+    )
+    .unwrap();
 }
