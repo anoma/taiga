8151e6cc6af9ea69076df548987f6a596b4081d0
Alberto Centelles <centelles.alberto@gmail.com>
2022-08-15 18:03:29 +0100

    Format code

--- src/doc_examples/user.rs
+++ src/doc_examples/user.rs
@@ -1,16 +1,16 @@
 use crate::circuit::circuit_parameters::CircuitParameters;
 use crate::circuit::gadgets::field_addition::field_addition_gadget;
 use crate::circuit::integrity::{
-    token_integrity_circuit,
-    ValidityPredicateInputNoteVariables, ValidityPredicateOutputNoteVariables,
+    token_integrity_circuit, ValidityPredicateInputNoteVariables,
+    ValidityPredicateOutputNoteVariables,
 };
 use crate::circuit::validity_predicate::{ValidityPredicate, NUM_NOTE};
 use crate::note::Note;
-use crate::token::{Token};
-use plonk_core::{circuit::Circuit, constraint_system::StandardComposer, prelude::Error};
-use ark_std;
+use crate::token::Token;
 use ark_ff::One;
 use ark_ff::Zero;
+use ark_std;
+use plonk_core::{circuit::Circuit, constraint_system::StandardComposer, prelude::Error};
 
 pub struct SendVP<CP: CircuitParameters> {
     // basic "private" inputs to the VP
@@ -45,20 +45,21 @@ where
         input_note_variables: &[ValidityPredicateInputNoteVariables],
         output_note_variables: &[ValidityPredicateOutputNoteVariables],
     ) -> Result<(), Error> {
-
         // * Alice does not want to send more than 3 XAN at a time.
         let mut rng = ark_std::test_rng();
         let xan_token =
             Token::<crate::circuit::circuit_parameters::PairingCircuitParameters>::dummy(&mut rng);
 
-
-        
-        let (xan_address_var, _) = token_integrity_circuit::<CP>(composer, &xan_token.token_vp.to_bits())?;
+        let (xan_address_var, _) =
+            token_integrity_circuit::<CP>(composer, &xan_token.token_vp.to_bits())?;
 
         // * Check that the token of all the notes of token XAN are less than 3 XAN
         // TODO: Look up a gadget in composer.rs that output a bit for checking an if statement
         for note_var in input_note_variables {
-            println!("note_token: {:?}, token: {:?}", note_var.token_addr, xan_address_var);
+            println!(
+                "note_token: {:?}, token: {:?}",
+                note_var.token_addr, xan_address_var
+            );
             composer.assert_equal(note_var.token_addr, xan_address_var);
             let x = note_var.value;
             let zero = composer.zero_var();
@@ -93,7 +94,6 @@ where
         1 << 17
     }
 }
-
 
 pub struct ReceiveVP<CP: CircuitParameters> {
     // basic "private" inputs to the VP
@@ -133,21 +133,25 @@ where
         let xan_token =
             Token::<crate::circuit::circuit_parameters::PairingCircuitParameters>::dummy(&mut rng);
 
-        let (xan_address_var, _) = token_integrity_circuit::<CP>(composer, &xan_token.token_vp.to_bits())?;
+        let (xan_address_var, _) =
+            token_integrity_circuit::<CP>(composer, &xan_token.token_vp.to_bits())?;
 
         // * Check that the token of all the notes of token XAN are less than 1 XAN
         for note_var in input_note_variables {
-        println!("note_token: {:?}, token: {:?}", note_var.token_addr, xan_address_var);
-        composer.assert_equal(note_var.token_addr, xan_address_var);
-        let x = note_var.value;
-        let zero = composer.zero_var();
-        let factor = CP::CurveScalarField::from(4u64);
-        println!("x: {:?}", x);
-        let output = composer.arithmetic_gate(|gate| {
-            gate.witness(x, zero, None)
-                .add(factor, CP::CurveScalarField::zero())
-        });
-        composer.range_gate(output, 2);
+            println!(
+                "note_token: {:?}, token: {:?}",
+                note_var.token_addr, xan_address_var
+            );
+            composer.assert_equal(note_var.token_addr, xan_address_var);
+            let x = note_var.value;
+            let zero = composer.zero_var();
+            let factor = CP::CurveScalarField::from(4u64);
+            println!("x: {:?}", x);
+            let output = composer.arithmetic_gate(|gate| {
+                gate.witness(x, zero, None)
+                    .add(factor, CP::CurveScalarField::zero())
+            });
+            composer.range_gate(output, 2);
         }
         Ok(())
     }
@@ -173,7 +177,6 @@ where
     }
 }
 
-
 #[test]
 fn test_user_creation() {
     use crate::circuit::circuit_parameters::CircuitParameters;
@@ -186,7 +189,7 @@ fn test_user_creation() {
     use crate::vp_description::ValidityPredicateDescription;
     use ark_poly_commit::PolynomialCommitment;
     use ark_std::test_rng;
-    use plonk_core::circuit::{verify_proof, VerifierData, Circuit};
+    use plonk_core::circuit::{verify_proof, Circuit, VerifierData};
 
     type Fr = <CP as CircuitParameters>::CurveScalarField;
     type P = <CP as CircuitParameters>::InnerCurve;
@@ -194,13 +197,17 @@ fn test_user_creation() {
 
     let mut rng = test_rng();
     let xan = Token::<CP>::dummy(&mut rng);
-    let send_input_notes = [(); NUM_NOTE].map(|_| Note::<CP>::dummy_of_range(xan.clone(), &mut rng,  3));
-    let send_output_notes = [(); NUM_NOTE].map(|_| Note::<CP>::dummy_of_range(xan.clone(), &mut rng, 3));
+    let send_input_notes =
+        [(); NUM_NOTE].map(|_| Note::<CP>::dummy_of_range(xan.clone(), &mut rng, 3));
+    let send_output_notes =
+        [(); NUM_NOTE].map(|_| Note::<CP>::dummy_of_range(xan.clone(), &mut rng, 3));
 
     let mut send_vp = SendVP::<CP>::new(send_input_notes, send_output_notes);
 
-    let receive_input_notes = [(); NUM_NOTE].map(|_| Note::<CP>::dummy_of_range(xan.clone(), &mut rng, 1));
-    let receive_output_notes = [(); NUM_NOTE].map(|_| Note::<CP>::dummy_of_range(xan.clone(), &mut rng, 1));
+    let receive_input_notes =
+        [(); NUM_NOTE].map(|_| Note::<CP>::dummy_of_range(xan.clone(), &mut rng, 1));
+    let receive_output_notes =
+        [(); NUM_NOTE].map(|_| Note::<CP>::dummy_of_range(xan.clone(), &mut rng, 1));
     let mut receive_vp = ReceiveVP::<CP>::new(receive_input_notes, receive_output_notes);
 
     // TODO: Run a setup. Export it to a file. Import it.
@@ -216,5 +223,4 @@ fn test_user_creation() {
         NullifierDerivingKey::<Fr>::rand(&mut rng),
     );
     let _alice_addr = alice.address().unwrap();
-
 }
