1e1e8baa813360bed5f80cd631105e706687d795
Song Xuyang <xuyangsong@hotmail.com>
2022-11-25 18:46:14 +0800

    improve map_to_curve and update the performance

--- taiga_halo2/src/circuit/hash_to_curve.rs
+++ taiga_halo2/src/circuit/hash_to_curve.rs
@@ -323,6 +323,10 @@ impl MapToCurveConfig {
             let alpha = meta.query_advice(self.alpha, Rotation::cur());
             // beta = inv0(div3)
             let beta = meta.query_advice(self.beta, Rotation::cur());
+            let ta = meta.query_advice(self.alpha, Rotation::next());
+            let num_x1 = meta.query_advice(self.beta, Rotation::next());
+            let div = meta.query_advice(self.gamma, Rotation::next());
+            let num_gx1 = meta.query_advice(self.delta, Rotation::next());
             // gamma = inv0(num_gx1)
             let gamma = meta.query_advice(self.gamma, Rotation::cur());
             let sqrt_a = meta.query_advice(self.x, Rotation::next());
@@ -331,6 +335,7 @@ impl MapToCurveConfig {
             let sqrt_b = meta.query_advice(self.y, Rotation::next());
             // epsilon = inv0(b - sqrt_b * sqrt_b)
             let epsilon = meta.query_advice(self.epsilon, Rotation::cur());
+            let gx1_square = meta.query_advice(self.epsilon, Rotation::next());
 
             let u_sgn0 = meta.query_advice(self.u_sgn0, Rotation::cur());
             let u_other_bits = meta.query_advice(self.u_other_bits, Rotation::cur());
@@ -352,15 +357,16 @@ impl MapToCurveConfig {
             let b = Expression::Constant(pallas::Iso::b());
             let z = Expression::Constant(pallas::Point::Z);
             let z_u2 = z.clone() * u.clone().square();
-            let ta = z_u2.clone().square() + z_u2.clone();
-            let num_x1 = b.clone() * (ta.clone() + one.clone());
+            let ta_poly = z_u2.clone().square() + z_u2.clone() - ta.clone();
+            let num_x1_poly = b.clone() * (ta.clone() + one.clone()) - num_x1.clone();
             let ta_is_zero = one.clone() - alpha * ta.clone();
             let poly1 = ta.clone() * ta_is_zero.clone();
-            let div = a.clone() * ternary(ta_is_zero, z, zero.clone() - ta);
+            let div_poly = a.clone() * ternary(ta_is_zero, z, zero.clone() - ta) - div.clone();
             let num2_x1 = num_x1.clone().square();
             let div2 = div.clone().square();
             let div3 = div2.clone() * div.clone();
-            let num_gx1 = (num2_x1 + a * div2) * num_x1.clone() + b * div3.clone();
+            let num_gx1_poly =
+                (num2_x1 + a * div2) * num_x1.clone() + b * div3.clone() - num_gx1.clone();
 
             // 5. x2 = Z * u^2 * x1
             let num_x2 = z_u2.clone() * num_x1.clone();
@@ -372,11 +378,11 @@ impl MapToCurveConfig {
             // let (gx1_square, y1) = F::sqrt_ratio(&num_gx1, &div3);
             let div3_is_zero = one.clone() - div3.clone() * beta.clone();
             let poly2 = div3.clone() * div3_is_zero.clone();
-            let a = beta * num_gx1.clone();
+            let a = beta.clone() * num_gx1.clone();
             let root_of_unity = Expression::Constant(pallas::Base::root_of_unity());
             let b = a.clone() * root_of_unity;
-            let num_gx1_is_zero = one.clone() - num_gx1.clone() * gamma;
-            let poly3 = num_gx1 * num_gx1_is_zero.clone();
+            let num_gx1_is_zero = one.clone() - num_gx1.clone() * gamma.clone();
+            let poly3 = num_gx1.clone() * num_gx1_is_zero.clone();
             let a_is_sqrt_value = a - sqrt_a.clone() * sqrt_a.clone();
             let a_is_sqrt = one.clone() - a_is_sqrt_value.clone() * delta;
             let poly4 = a_is_sqrt_value * a_is_sqrt.clone();
@@ -390,13 +396,14 @@ impl MapToCurveConfig {
             let two = Expression::Constant(pallas::Base::from(2));
             let a_is_square_xor_b_is_square =
                 a_is_sqrt.clone() + b_is_sqrt.clone() - two.clone() * a_is_sqrt.clone() * b_is_sqrt;
-            let poly6 = (one.clone() - num_gx1_is_zero.clone())
-                * (one.clone() - div3_is_zero.clone())
+            let poly6 = (num_gx1 * gamma)
+                * (div3.clone() * beta)
                 * (one.clone() - a_is_square_xor_b_is_square);
 
             // gx1_square = a_is_sqrt & !(!num_gx1_is_zero & div3_is_zero)
-            let gx1_square =
-                a_is_sqrt.clone() * (one.clone() - (one - num_gx1_is_zero) * div3_is_zero);
+            let gx1_square_poly = a_is_sqrt.clone()
+                * (one.clone() - (one - num_gx1_is_zero) * div3_is_zero)
+                - gx1_square.clone();
             let y1 = ternary(a_is_sqrt, sqrt_a, sqrt_b);
             let theta = Expression::Constant(pallas::Point::THETA);
             let y2 = theta * z_u2 * u.clone() * y1.clone();
@@ -419,10 +426,15 @@ impl MapToCurveConfig {
                 q_map_to_curve,
                 [
                     ("ta is zero", poly1),
+                    ("ta", ta_poly),
+                    ("num_x1", num_x1_poly),
+                    ("div", div_poly),
                     ("div3 is zero", poly2),
+                    ("num_gx1", num_gx1_poly),
                     ("num_gx1 is zero", poly3),
                     ("a is sqrt", poly4),
                     ("b is sqrt", poly5),
+                    ("gx1_square", gx1_square_poly),
                     (
                         "num_gx1_is_zero | div3_is_zero | (a_is_sqrt ^ b_is_square)",
                         poly6,
@@ -456,20 +468,24 @@ impl MapToCurveConfig {
         });
         let alpha = ta.map(|ta| ta.invert().unwrap_or(pallas::Base::zero()));
         region.assign_advice(|| "alpha", self.alpha, offset, || alpha)?;
+        region.assign_advice(|| "ta", self.alpha, offset + 1, || ta)?;
 
         let a = pallas::Iso::a();
         let b = pallas::Iso::b();
         let div = ta.map(|ta| a * pallas::Base::conditional_select(&-ta, &z, ta.is_zero()));
+        region.assign_advice(|| "div", self.gamma, offset + 1, || div)?;
         let div3 = div.map(|div| div.square() * div);
 
         let beta = div3.map(|v| v.invert().unwrap_or(pallas::Base::zero()));
         region.assign_advice(|| "beta", self.beta, offset, || beta)?;
 
         let num_x1 = ta.map(|ta| b * (ta + pallas::Base::one()));
+        region.assign_advice(|| "num_x1", self.beta, offset + 1, || num_x1)?;
         let num_gx1 = num_x1
             .zip(div)
             .zip(div3)
             .map(|((num_x1, div), div3)| (num_x1.square() + a * div.square()) * num_x1 + b * div3);
+        region.assign_advice(|| "num_gx1", self.delta, offset + 1, || num_gx1)?;
         let gamma = num_gx1.map(|v| v.invert().unwrap_or(pallas::Base::zero()));
         region.assign_advice(|| "gamma", self.gamma, offset, || gamma)?;
 
@@ -523,15 +539,18 @@ impl MapToCurveConfig {
                     let num_x2 = z_u2 * num_x1;
                     let num_x = pallas::Base::conditional_select(&num_x2, &num_x1, gx1_square);
                     let y = pallas::Base::conditional_select(&y2, &y1, gx1_square);
-                    (num_x, y)
+                    (gx1_square, num_x, y)
                 });
 
-        let y_sgn0 = xy.map(|(_, y)| pallas::Base::from((y.to_repr()[0] & 1) as u64));
+        let gx1_square = xy.map(|(gx1_square, _, _)| pallas::Base::from(bool::from(gx1_square)));
+        region.assign_advice(|| "gx1_square", self.epsilon, offset + 1, || gx1_square)?;
+
+        let y_sgn0 = xy.map(|(_, _, y)| pallas::Base::from((y.to_repr()[0] & 1) as u64));
         region.assign_advice(|| "y_sgn0", self.u_sgn0, offset + 1, || y_sgn0)?;
 
         let y_other_bits = xy
             .zip(y_sgn0)
-            .map(|((_, y), y_sgn0)| (y - y_sgn0) * pallas::Base::from(2).invert().unwrap());
+            .map(|((_, _, y), y_sgn0)| (y - y_sgn0) * pallas::Base::from(2).invert().unwrap());
         region.assign_advice(
             || "y_other_bits",
             self.u_other_bits,
@@ -539,8 +558,8 @@ impl MapToCurveConfig {
             || y_other_bits,
         )?;
 
-        let x = xy.zip(div).map(|((x, _), div)| x * div);
-        let y = xy.zip(div3).zip(u.value()).map(|(((_, y), div3), u)| {
+        let x = xy.zip(div).map(|((_, x, _), div)| x * div);
+        let y = xy.zip(div3).zip(u.value()).map(|(((_, _, y), div3), u)| {
             let y = pallas::Base::conditional_select(&(-y), &y, u.is_odd().ct_eq(&y.is_odd()));
             y * div3
         });
@@ -889,7 +908,46 @@ fn test_hash_to_curve_circuit() {
     let circuit = MyCircuit {};
 
     let prover = MockProver::run(11, &circuit, vec![]).unwrap();
-    assert_eq!(prover.verify(), Ok(()))
+    assert_eq!(prover.verify(), Ok(()));
+
+    // TODO: there is still space to improve the performance. keep the test
+    // {
+    //     use halo2_proofs::{
+    //         dev::MockProver,
+    //         plonk::{create_proof, keygen_pk, keygen_vk, verify_proof, SingleVerifier},
+    //         poly::commitment::Params,
+    //         transcript::{Blake2bRead, Blake2bWrite},
+    //     };
+    //     use pasta_curves::vesta;
+    //     use rand::rngs::OsRng;
+
+    //     let mut rng = OsRng;
+    //     let params = Params::new(11);
+    //     let empty_circuit = MyCircuit {};
+    //     let vk = keygen_vk(&params, &empty_circuit).expect("keygen_vk should not fail");
+    //     let pk = keygen_pk(&params, vk, &empty_circuit).expect("keygen_pk should not fail");
+    //     let mut transcript = Blake2bWrite::<_, vesta::Affine, _>::init(vec![]);
+    //     use std::time::Instant;
+    //     let start = Instant::now();
+    //     create_proof(
+    //         &params,
+    //         &pk,
+    //         &[empty_circuit],
+    //         &[&[]],
+    //         &mut rng,
+    //         &mut transcript,
+    //     )
+    //     .unwrap();
+    //     let proof = transcript.finalize();
+    //     println!(
+    //         "hash to curve time: {:?}",
+    //         Instant::now().duration_since(start)
+    //     );
+
+    //     let strategy = SingleVerifier::new(&params);
+    //     let mut transcript = Blake2bRead::init(&proof[..]);
+    //     assert!(verify_proof(&params, pk.get_vk(), strategy, &[&[]], &mut transcript).is_ok());
+    // }
 }
 
 #[test]
