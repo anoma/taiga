822f881b4b99362ffe608bd8d3b44b9a9c5220b2
therealyingtong <yingtong.lai@gmail.com>
2023-10-12 16:12:07 +0800

    tx_examples::token: Use Token type in methods

--- taiga_halo2/examples/tx_examples/token.rs
+++ taiga_halo2/examples/tx_examples/token.rs
@@ -7,8 +7,8 @@ use taiga_halo2::{
     circuit::vp_examples::{
         signature_verification::COMPRESSED_TOKEN_AUTH_VK,
         token::{
-            generate_input_token_note_proving_info, generate_output_token_note_proving_info,
-            transfrom_token_name_to_token_property, TokenAuthorization, COMPRESSED_TOKEN_VK,
+            generate_input_token_note_proving_info, generate_output_token_note_proving_info, Token,
+            TokenAuthorization, COMPRESSED_TOKEN_VK,
         },
     },
     constant::TAIGA_COMMITMENT_TREE_DEPTH,
@@ -20,20 +20,19 @@ use taiga_halo2::{
 
 pub fn create_random_token_note<R: RngCore>(
     mut rng: R,
-    name: &str,
-    value: u64,
+    token: &Token,
     rho: Nullifier,
     nk_container: NullifierKeyContainer,
     auth: &TokenAuthorization,
 ) -> Note {
-    let app_data_static = transfrom_token_name_to_token_property(name);
+    let app_data_static = token.encode_name();
     let app_data_dynamic = auth.to_app_data_dynamic();
     let rseed = RandomSeed::random(&mut rng);
     Note::new(
         *COMPRESSED_TOKEN_VK,
         app_data_static,
         app_data_dynamic,
-        value,
+        token.value(),
         nk_container,
         rho,
         true,
@@ -44,12 +43,10 @@ pub fn create_random_token_note<R: RngCore>(
 #[allow(clippy::too_many_arguments)]
 pub fn create_token_swap_ptx<R: RngCore>(
     mut rng: R,
-    input_token: &str,
-    input_value: u64,
+    input_token: Token,
     input_auth_sk: pallas::Scalar,
     input_nk: NullifierKeyContainer, // NullifierKeyContainer::Key
-    output_token: &str,
-    output_value: u64,
+    output_token: Token,
     output_auth_pk: pallas::Point,
     output_nk_com: NullifierKeyContainer, // NullifierKeyContainer::Commitment
 ) -> ShieldedPartialTransaction {
@@ -57,22 +54,14 @@ pub fn create_token_swap_ptx<R: RngCore>(
 
     // input note
     let rho = Nullifier::from(pallas::Base::random(&mut rng));
-    let input_note = create_random_token_note(
-        &mut rng,
-        input_token,
-        input_value,
-        rho,
-        input_nk,
-        &input_auth,
-    );
+    let input_note = create_random_token_note(&mut rng, &input_token, rho, input_nk, &input_auth);
 
     // output note
     let input_note_nf = input_note.get_nf().unwrap();
     let output_auth = TokenAuthorization::new(output_auth_pk, *COMPRESSED_TOKEN_AUTH_VK);
     let output_note = create_random_token_note(
         &mut rng,
-        output_token,
-        output_value,
+        &output_token,
         input_note_nf,
         output_nk_com,
         &output_auth,
@@ -96,7 +85,7 @@ pub fn create_token_swap_ptx<R: RngCore>(
     let input_note_proving_info = generate_input_token_note_proving_info(
         &mut rng,
         input_note,
-        input_token.to_string(),
+        input_token.name(),
         input_auth,
         input_auth_sk,
         merkle_path.clone(),
@@ -108,7 +97,7 @@ pub fn create_token_swap_ptx<R: RngCore>(
     let output_note_proving_info = generate_output_token_note_proving_info(
         &mut rng,
         output_note,
-        output_token.to_string(),
+        output_token.name(),
         output_auth,
         input_notes,
         output_notes,
